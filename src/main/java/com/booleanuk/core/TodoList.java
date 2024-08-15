0956eaaef8b612b0379280cf3c512b67aab0579c
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-15 14:00:10 +0200

    TodoList addTask and removeTask

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,39 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
+
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    public boolean addTask(String name){
+        Task task = new Task(name);
+        tasks.add(task);
+
+        return true;
+    }
+
+    public String removeTask(String findTask){
+        for(int i = 0; i < tasks.size(); i++){
+            if(tasks.get(i).getName() == findTask){
+                tasks.remove(tasks.get(i));
+
+                return "Removed";
+            }
+        }
+
+        return "Not in list";
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
