fd5129d15fe35c474c3657b10d95045e489e8e1a
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-15 11:51:41 +0200

    Changed name to TodoListTest

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -4,10 +4,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
+
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void addTaskTest() {
+        TodoList todoList = new TodoList();
+        Assertions.assertTrue(todoList.addTask("name"));
+
+    }
+
+    @Test
+    public void removeTaskTest(){
+        TodoList todoList = new TodoList();
+
+        //Will fail because of jumping not in list
+        //todoList.addTask("jumping");
+        todoList.addTask("sprinting");
+
+        Assertions.assertEquals("Removed", todoList.removeTask("jumping"));
     }
 }
+
