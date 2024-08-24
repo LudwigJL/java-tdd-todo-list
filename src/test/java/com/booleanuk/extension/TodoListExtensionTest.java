c3effb8aef9f9b4599bce1c3b03a6ff31afece35
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-24 14:23:43 +0200

    red test changeStatusByUsingIDTest

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -33,5 +33,28 @@ public class TodoListExtensionTest {
         Assertions.assertEquals("Dishing", todoList.getTask(0).getName());
     }
 
+    @Test
+    public void changeStatusByUsingIDTest(){
+        TodoList todoList = new TodoList();
+
+
+        //Should be set as false when adding
+        todoList.addTask("Running");
+        Assertions.assertEquals(false, todoList.getTask(0).getComplete());
+
+        //Should change the status(complete) with this method, not yet implemented.
+        todoList.changeComplete(0);
+
+        //Should now have changed to true
+        Assertions.assertEquals(true, todoList.getTask(0).getComplete());
+
+
+    }
+
+    @Test
+    public void seeDateAndTimeOfWhenTaskWasCreatedTest(){
+
+    }
+
 
 }
