fd56587c2319f79aa34b8940fdfc20d782e88bd4
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-24 14:17:17 +0200

    red test changeNameByIDTest()

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -16,5 +16,22 @@ public class TodoListExtensionTest {
         Assertions.assertEquals(todoList.getTask(1).getName(), "Running");
     }
 
+    @Test
+    public void changeNameByIDTest(){
+        TodoList todoList = new TodoList();
+
+        //This will have ID 0 in the hashmap
+        todoList.addTask("Swimming");
+
+        //This will be at 1.
+        todoList.addTask("Running");
+
+        //Now I want to change Swimming to Dishing.
+        todoList.changeName(0, "Dishing");
+
+        //Now the name of ID 0 will be changed from swimming to dishing. This will fail because method not yet implemented
+        Assertions.assertEquals("Dishing", todoList.getTask(0).getName());
+    }
+
 
 }
